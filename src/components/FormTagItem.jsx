16b408820eeb71c28b6ce662e1c6afcf034deaa4
Xiayucheng1212 <x1204585568@gmail.com>
2024-02-29 21:10:49 +0800

    UI Fix: FormTagItem

--- src/components/FormTagItem.jsx
+++ src/components/FormTagItem.jsx
@@ -17,7 +17,7 @@ const FormTagItem = (props) => {
   useEffect(() => {
     props.register('tags', { value: selectedTags })
     props.setValue('tags', selectedTags);
-}, [props.setValue, props.register, selectedTags]);
+  }, [props.setValue, props.register, selectedTags]);
 
   const handleTagClick = (name) => {
     if (selectedTags.find((tag) => tag === name)) return;
@@ -53,19 +53,17 @@ const FormTagItem = (props) => {
         {props.label}
       </div>
       <div className="self-stretch h-[30px] pl-[10px] pr-2.5 py-[5px] bg-zinc-300 rounded-t-[5px] flex space-x items-center">
-        <div className="w-[230px] h-[24px] flex mr-[10px] bg-transparent bg-zinc-300 justify-start items-center gap-x-2 overflow-x-scroll">
-          {selectedTags.length === 0 ? (
-            <input
-              className="bg-transparent resize-none text-neutral-600 text-xs font-medium font-['Jost']"
-              disabled
-              placeholder="Click to select"
-            />
-          ) : (
-            selectedTags.map((tag, i) => (
-              <SelectedTag key={i} name={tag} onClick={handleClearOne} />
-            ))
-          )}
-        </div>
+        {selectedTags.length === 0 ? (
+          <input
+            className="bg-transparent resize-none text-neutral-600 text-xs font-medium font-['Jost']"
+            disabled
+            placeholder="Click to select"
+          />
+        ) : (
+          selectedTags.map((tag, i) => (
+            <SelectedTag key={i} name={tag} onClick={handleClearOne} />
+          ))
+        )}
         <div className="w-3 h-3" onClick={handleClearAll}>
           <img
             className="w-3 h-3 justify-center items-center"
@@ -76,7 +74,7 @@ const FormTagItem = (props) => {
       </div>
       <div className="w-full flex flex-wrap p-[10px] bg-white rounded-b-[5px] justify-start items-center gap-2">
         {tags.map((tag, i) => (
-            <Tag key={i} name={tag} onClick={handleTagClick} />
+          <Tag key={i} name={tag} onClick={handleTagClick} />
         ))}
         <div className="w-full h-[30px] pl-[10px] pr-2.5 py-[5px] bg-zinc-300 rounded-[5px] justify-end items-center inline-flex">
           <input
