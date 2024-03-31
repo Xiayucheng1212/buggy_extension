b9359b4fafd735eb6c654508b24c0dd72ae30968
109062335 <winston0724@gmail.com>
2024-04-01 01:42:42 +0800

    try to implement search but not success

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,34 +1,19 @@
 /* global chrome */
 import React, { useState, useEffect, useContext } from "react";
-import draft_logo from "../imgs/draft.png";
-import add_logo from "../imgs/add_icon.png";
-import Button from "./Button";
-import SearchBar from "./SearchBar";
+
 import TagItem from "./TagItem";
-import { Link } from "react-router-dom";
 import DBContext from "../DBContext";
 import TagController from "../controller/TagController";
 import LinkItem from "./LinkItem";
 import LinkController from "../controller/LinkController";
 import EmptyInformation from "./EmptyInformation";
-import DraftCountNotification from "./DraftCountNotification";
 
-const HomePage = () => {
+const HomePage = (props) => {
 	const [tags, setTags] = useState([]);
 	const [searching, setSearching] = useState(false);
 	const [keyword, setKeyword] = useState("");
 	const [searchedLinks, setSearchedLinks] = useState([]);
-	const [draftCount, setDraftCount] = useState(0);
 	const dbProm = useContext(DBContext).dbProm;
-
-	useEffect(() => {
-		if(!chrome.action) return;
-		chrome.action.getBadgeText({}, (badgeText) => {
-			console.log(badgeText);
-            let count = parseInt(badgeText) || 0;
-            setDraftCount(count);
-        });
-	}, []);
 
 	useEffect(() => {
 		dbProm.then((db) => {
@@ -61,18 +46,16 @@ const HomePage = () => {
 	};
 
 	return (
-		<>
-			<div className="self-stretch bg-white flex-col justify-start items-center gap-1.5 inline-flex">
-				{searching
-					? (searchedLinks.length ? searchedLinks.map((link, i) => (
-						<LinkItem key={i} id={link.id} name={link.name} url={link.url} />
-					)) :
-						<EmptyInformation information={"No results found."} />)
-					: tags.map((tag, i) => (
-						<TagItem key={i} name={tag.name} links={tag.links} />
-					))}
-			</div>
-		</>
+		<div className="self-stretch bg-white flex-col justify-start items-center gap-1.5 inline-flex">
+			{searching
+				? (searchedLinks.length ? searchedLinks.map((link, i) => (
+					<LinkItem key={i} id={link.id} name={link.name} url={link.url} />
+				)) :
+					<EmptyInformation information={"No results found."} />)
+				: tags.map((tag, i) => (
+					<TagItem key={i} name={tag.name} links={tag.links} />
+				))}
+		</div>
 	);
 };
 
