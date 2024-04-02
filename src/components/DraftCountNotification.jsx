52a5d0ccdfed89c7485e7c111bec4d0f860b1c59
Xiayucheng1212 <x1204585568@gmail.com>
2024-04-03 00:41:35 +0800

    Fix draft count overflow

--- src/components/DraftCountNotification.jsx
+++ src/components/DraftCountNotification.jsx
@@ -4,8 +4,8 @@ const DraftCountNotification = (props) => {
     return (
         <>
             {props.draftCount ? 
-                <div className="absolute -top-1 -right-1 h-[15px] w-[15px] rounded-[5px] flex justify-center items-center" style={{ backgroundColor: "#8AB4F8" }}>
-                    <div className="select-none text-[10px]">{props.draftCount}</div>
+                <div className="absolute -top-1 -right-1 rounded-[5px] flex justify-center items-center" style={{ backgroundColor: "#8AB4F8" }}>
+                    <div className=" text-[10px] px-[5px]">{props.draftCount}</div>
                 </div>
                 : null}
         </>
