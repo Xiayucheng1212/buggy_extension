4cb4349b2fdf72398f18857ba222414570a1a88f
Xiayucheng1212 <x1204585568@gmail.com>
2024-04-03 12:20:55 +0800

    Fix text overflow

--- src/components/DraftItem.jsx
+++ src/components/DraftItem.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import up_logo from '../imgs/up-arrow.png';
 import delete_logo from '../imgs/delete.png';
 import CopyToClipboardButton from './CopyToClipboardButton';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const DraftItem = (props) => {
+    const navigate = useNavigate();
 
+    const handleClick = () => {
+        navigate(`/edit/${props.id}`);
+    }
     return (
-        <div className="w-full h-[35px] pl-1.5 justify-start items-center inline-flex">
-            <div className="w-[200px] h-[30px] pr-[10px] flex-col justify-center items-start inline-flex">
-                <div className="text-neutral-600 text-xs font-bold font-['Jost']">{props.name}</div>
-                <div className="text-neutral-600 text-[10px] font-medium font-['Jost'] whitespace-nowrap">{props.url}</div>
+        <div className="w-full h-[35px] pl-1.5 justify-start items-center inline-flex hover:bg-zinc-200">
+            <div className="w-[200px] h-[30px] pr-[10px] flex-col justify-center items-start inline-flex cursor-pointer" onClick={ handleClick}>
+                <div className="w-[190px] text-neutral-600 text-xs font-bold font-['Jost'] truncate hover:text-clip">{props.name}</div>
+                <div className="w-[190px] text-neutral-600 text-[10px] font-medium font-['Jost'] truncate hover:text-clip">{props.url}</div>
             </div>
             <div className="w-[80px] h-[30px] flex justify-center items-center space-x-2">
                 <CopyToClipboardButton url={props.url} />
